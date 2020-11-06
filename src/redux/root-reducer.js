3c9bd253d9a69b4a3993f8bf0b425b82d129d7a9
Yassine <yassin23raki@gmail.com>
2020-11-07 00:30:54 +0100

    moved directory state from directory component to a directory reducer in redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,21 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from '../redux/directory/directory.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
