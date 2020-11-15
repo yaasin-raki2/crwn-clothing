fe40254c2122caeabf25e804e457421481c5a747
Yassine <yassin23raki@gmail.com>
2020-11-15 23:31:01 +0100

    transforming checking for user session functionality to saga

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -8,6 +8,7 @@ import {
   auth,
   googleProvider,
   createUserProfileDocument,
+  getCurrentUser,
 } from '../../firebase/firebase.utils';
 import UserActionTypes from './user.types';
 
@@ -39,6 +40,16 @@ function* signInWithEmail({ payload: { email, password } }) {
   }
 }
 
+function* isUserAuthenticated() {
+  try {
+    const userAuth = yield getCurrentUser();
+    if (!userAuth) return;
+    yield getSnapshotFromUserAuth(userAuth);
+  } catch (error) {
+    yield put(signInFailure(error));
+  }
+}
+
 export function* onGoogleSignInStart() {
   yield takeLatest(UserActionsTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
@@ -47,6 +58,14 @@ export function* onEmailSignInStart() {
   yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_START, signInWithEmail);
 }
 
+export function* onCheckUserSession() {
+  yield takeLatest(UserActionTypes.CHECK_USER_SESSION, isUserAuthenticated);
+}
+
 export function* userSagas() {
-  yield all([call(onGoogleSignInStart), call(onEmailSignInStart)]);
+  yield all([
+    call(onGoogleSignInStart),
+    call(onEmailSignInStart),
+    call(onCheckUserSession),
+  ]);
 }
