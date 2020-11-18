bb9f4995972e1eea6f486f311e2f3f39b32b78bb
Yassine <yassin23raki@gmail.com>
2020-11-19 00:46:44 +0100

    converting the collection component from using redux saga to get the shop data to use context api with hooks

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
-import { connect } from 'react-redux';
-import { selectCollection } from '../../redux/shop/shop.selectors';
+import CollectionsContext from '../../contexts/collections/collections.context';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ collection: { title, items } }) => (
-  <div className='collection-page'>
-    <h2>{title}</h2>
-    <div className='items'>
-      {items.map(item => (
-        <CollectionItem key={item.id} item={item} />
-      ))}
+const CollectionPage = ({ match }) => {
+  const collections = useContext(CollectionsContext);
+  const collection = collections[match.params.collectionId];
+  const { title, items } = collection;
+  return (
+    <div className='collection-page'>
+      <h2>{title}</h2>
+      <div className='items'>
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state),
-});
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
